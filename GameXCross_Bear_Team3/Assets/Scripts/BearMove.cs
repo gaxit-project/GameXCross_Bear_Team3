68a4d54fce05925994a0521fd140bb45b9c70f5e
leon <e1304025080@oit.ac.jp>
2025-11-13 15:00:22 +0900

    2025/11/13 14:59 廣部 指定した円周上にCubeが出現するようにした

--- GameXCross_Bear_Team3/Assets/Scripts/BearMove.cs
+++ GameXCross_Bear_Team3/Assets/Scripts/BearMove.cs
@@ -2,17 +2,52 @@ using UnityEngine;
 
 public class BearMove : MonoBehaviour
 {
+    [Header("ŒF‚Ì‘¬“x")]
     public float moveSpeed = 5.0f;
 
-    private float bearMove;
+    [Header("oŒ»Ý’è")]
+    public float radius = 25.0f;
+    public Vector3 centerPoint = Vector3.zero;
+
+    private Vector3 moveDirection;
 
     void Start()
     {
-        
+        ReapawnAndSetDirection();
     }
 
     void Update()
     {
-        transform.position += Vector3.back * moveSpeed * Time.deltaTime;
+        transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+        if(moveDirection != Vector3.zero)
+        {
+            transform.rotation = Quaternion.LookRotation(moveDirection);
+        }
+
+        Vector3 posOnPlane = new Vector3(transform.position.x, centerPoint.y, transform.position.z);
+        float currentDistance = Vector3.Distance(posOnPlane, centerPoint);
+
+        if (currentDistance > radius + 0.1f) ReapawnAndSetDirection();
+
+    }
+
+    private void ReapawnAndSetDirection()
+    {
+        float randomAngle = Random.Range(0f, 2f * Mathf.PI);
+
+        float x = centerPoint.x + radius * Mathf.Cos(randomAngle);
+        float z = centerPoint.z + radius * Mathf.Sin(randomAngle);
+
+        float y = centerPoint.y;
+
+        Vector3 spawnPosition = new Vector3(x, y, z);
+
+        transform.position = spawnPosition;
+
+        Vector3 centerOnPlane = new Vector3(centerPoint.x, y, centerPoint.z);
+        Vector3 directionToCenter = (centerOnPlane - spawnPosition).normalized;
+
+        moveDirection = directionToCenter;
     }
 }
