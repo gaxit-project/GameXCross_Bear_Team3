fd497ade54c214bb7be2e801ab3e2882a17679dd
Falcon-8810 <hayate.moriyama@icloud.com>
2026-01-20 22:20:50 +0900

    feat;フェイズリザルトのスクリプトを書いた．キルカウンターから値を読み取って表示させます．表示場所をまだちゃんと作ってないので表示は変です，

--- GameXCross_Bear_Team3/Assets/Scripts/System/PhaseResult.cs
+++ GameXCross_Bear_Team3/Assets/Scripts/System/PhaseResult.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using TMPro;
 
 public class PhaseResult : MonoBehaviour
 {
@@ -7,6 +8,19 @@ public class PhaseResult : MonoBehaviour
     [SerializeField] private GameObject phaseResult;
     [SerializeField] private GameObject button;
     [SerializeField] private GameObject selectUIbutton;
+
+    [Header("ï\é¶")]
+    [SerializeField] private TextMeshProUGUI day;
+    [SerializeField] private TextMeshProUGUI kill;
+    [SerializeField] private TextMeshProUGUI capture;
+    [SerializeField] private TextMeshProUGUI damage;
+
+
+    private int daycount = 1;
+    private int killcount = 0;
+    private int capturecount = 0;
+    private int damagecount = 0;
+
 
     public static PhaseResult Instance { get; private set; }
 
@@ -28,10 +42,26 @@ public class PhaseResult : MonoBehaviour
         
         phaseResult.SetActive(true);
         EventSystem.current.SetSelectedGameObject(button);
+
+        killcount = KillCountManager.Instance.GetKillCount(daycount);
+        capturecount = KillCountManager.Instance.GetCaptureCount(daycount);
+        damagecount = KillCountManager.Instance.GetDamageCount(daycount);
+        resulttype();
+
     }
+
+    private void resulttype()
+    {
+        day.text = daycount + "ì˙ñ⁄íÜä‘ïÒçê"; 
+        kill.text = "" + killcount;
+        capture.text = "" + capturecount;
+        damage.text = "" + damagecount;
+    }
+
     public void NextDay()
     {
         phaseResult.SetActive(false);
+        daycount++;
         GameManager.Instance.StartSetupPhase();
         EventSystem.current.SetSelectedGameObject(selectUIbutton);
     }
