b0489392b48f5aef89265fa34e955ebc33e5b977
LEON-72 <hr.leon72@gmail.com>
2026-01-30 18:34:17 +0900

    fix: ハンターが柵を貫通しないように修正

--- GameXCross_Bear_Team3/Assets/Scripts/System/GameManager.cs
+++ GameXCross_Bear_Team3/Assets/Scripts/System/GameManager.cs
@@ -134,7 +134,7 @@ public class GameManager : MonoBehaviour
     {
         CurrentState.Value = GameState.Setup;
         TimeRemaining.Value = SetupTime;
-        pointerController.UIsettrue();
+        // pointerController.UIsettrue();
 
         // 報酬の支払い
         if (_pendingIncome > 0)
@@ -167,7 +167,7 @@ public class GameManager : MonoBehaviour
 
     private void StartBattlePhase()
     {
-        pointerController.UIsetfalse();
+        // pointerController.UIsetfalse();
         CurrentState.Value = GameState.Battle;
         TimeRemaining.Value = 0;
         _isWaveSpawningComplete = true;
